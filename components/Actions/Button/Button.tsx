ec4c6076960b6b122bf7937bb7bd14f74cb0991a
Vítor <caulicons@hotmail.com>
2023-10-02 13:44:31 -0300

    fix: button active border color

--- components/Actions/Button/Button.tsx
+++ components/Actions/Button/Button.tsx
@@ -3,33 +3,36 @@ import { cva, type VariantProps } from "cva";
 type buttonProps = {} & React.HTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof style>;
 
-const style = cva("p-4 rounded-lg active:border active:border-4 leading-6 cursor-pointer", {
-  variants: {
-    variant: {
-      filled: [
-        "text-darkBlue bg-tertiary",
-        "hover:bg-secondary",
-        "active:border-tertiary",
-        "focus:bg-primary",
-      ],
-      outline: [
-        "text-white bg-tertiary/5",
-        "hover:bg-tertiary/10",
-        "active:border-tertiary/5",
-        "focus:bg-tertiary/20",
-      ]
+const style = cva(
+  "p-4 rounded-lg active:border active:border-4 leading-6 cursor-pointer",
+  {
+    variants: {
+      variant: {
+        filled: [
+          "text-darkBlue bg-tertiary",
+          "hover:bg-secondary",
+          "active:bg-secondary active:border-tertiary",
+          "focus:bg-primary",
+        ],
+        outline: [
+          "text-white bg-tertiary/5",
+          "hover:bg-tertiary/10",
+          "active:border-tertiary/5",
+          "focus:bg-tertiary/20",
+        ],
+      },
+    },
+
+    defaultVariants: {
+      variant: "filled",
     },
   },
-  
-  defaultVariants: {
-    variant: "filled",
-  },
-});
+);
 
 const Button = ({ variant, children, className, ...props }: buttonProps) => {
   return (
     <button className={`${style({ variant })} ${className}`} {...props}>
-      {children}
+      {children ?? "Button"}
     </button>
   );
 };
